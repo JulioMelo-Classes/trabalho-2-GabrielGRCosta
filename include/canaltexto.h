3adbf9fe2ab32936c93a5ad8e1a7f70118cd2ed7
GabrielGRCosta <galvaogabriel890@gmail.com>
2021-07-24 18:22:14 +0000

    magica

---
+++ include/canaltexto.h
@@ -0,0 +1,13 @@
+#ifndef CANALTEXTO_H
+#define CANALTEXTO_H
+#include <string>
+#include <vector>
+#include "mensagem.h"
+using namespace std;
+
+class CanalTexto{
+	string nome;
+	vector <mensagem> mensagens;
+};
+
+#endif
---
+++ include/mensagem.h
@@ -0,0 +1,11 @@
+#ifndef MENSAGEM_H
+#define MENSAGEM_H
+#include <string>
+
+class mensagem{
+	string dataHora;
+	int enviadaPor;
+	string conteudo;
+};
+
+#endif
