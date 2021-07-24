afb3da66c55607a3c0e5946727715589a0ecc1e4
GabrielGRCosta <galvaogabriel890@gmail.com>
2021-07-24 15:24:14 +0000

    aloha

--- include/servidor.h
+++ include/servidor.h
@@ -5,6 +5,7 @@
 using namespace std;
 
 class Servidor{
+	private:
 	int usuarioDonoId;	
 	string nome;
 	string descricao;
@@ -12,6 +13,22 @@ class Servidor{
 	vector <string> CanaisTexto;
 	vector <int> participantesIDs; 
 
+  public:
+	
+	void set_nameserver(string n);
+
+	void set_id(int a);
+
+	void set_descri(string b);
+
+	void set_convite(string k);
+
+	string get_nameserver();
+
+	int get_donoid();
+
+
 };
+
 
 #endif
